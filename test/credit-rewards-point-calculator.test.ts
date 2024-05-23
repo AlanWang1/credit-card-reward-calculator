2bdde603459a57a0fb29a55410e42a113f1be7dd
Alan Wang <alan.wang9018@gmail.com>
2024-05-22 21:30:33 -0400

    Initial commit

---
+++ test/credit-rewards-point-calculator.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CreditRewardsPointCalculator from '../lib/credit-rewards-point-calculator-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/credit-rewards-point-calculator-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CreditRewardsPointCalculator.CreditRewardsPointCalculatorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
